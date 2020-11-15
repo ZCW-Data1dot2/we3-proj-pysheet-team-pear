292c105b32a1ce4fd3212a5666ec77ed2605734f
alfonso <alfonso@zipcodes-MacBook-Pro.local>
2020-11-15 10:58:50 -0500

    latest changes-AG

--- pysheet/pysheet.py
+++ pysheet/pysheet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import tkinter as tk
 import math
 import re
@@ -13,20 +12,23 @@ cellre = re.compile(r'\b[A-Z][0-9]\b')
 
 def cellname(col, row):
     # returns a string translates col 0, row 0 to 'A1'
-    pass
+    # uses ascii to find letter equivalent to number
+    return chr(ord('A') + col) + str(row + 1)
 
 
 class Cell():
     def __init__(self, row, col, siblings, parent):
-        # save off instance variables from arguments
-        # and also
-        #set name to cellname(i, j)
-        # set value of cell to zero
-        # set formula to a str(value)
-        # Set of Dependencies - must be updated if this cell changes
-        # make deps empty
-        # Set of Requirements - values required for this cell to calculate
-        # make reqs empty
+        self.row = row
+        self.col = col
+        self.name = cellname(col, row)
+        self.siblings = siblings
+        self.parent = parent
+
+        self.value = 0
+        self.formula = str(self.value)
+
+        self.depenencies = set()
+        self.requirements = set()
 
         # be happy you get this machinery for free.
         self.var = tk.StringVar()
@@ -43,6 +45,7 @@ class Cell():
 
         # set this cell's var to cell's value
         # and you're done.
+        self.var.set(self.value)
 
     def move(self, rowadvance, coladvance):
         targetrow = (self.row + rowadvance) % Nrows
@@ -57,29 +60,31 @@ class Cell():
     def calculate(self):
         # find all the cells mentioned in the formula.
         #  put them all into a tmp set currentreqs
-        #  
+        currentreqs = set(cellre.findall(self.formula))
         # Add this cell to the new requirement's dependents
         # removing all the reqs that we might no longer need
-        # for each in currentreqs - self.reqs
-        #    my siblings[].deps.add(self.name)
+        for each in currentreqs - self.reqs:
+            my siblings[each].deps.add(self.name)
         # Add remove this cell from dependents no longer referenced
-        # for each in self.reqs - currentreqs:
-        #    my siblings[r].deps.remove(self.name)
-        #  
+        for each in self.reqs - currentreqs:
+            my siblings[each].deps.remove(self.name)
+        #
         # Look up the values of our required cells
-        # reqvalues = a comprehension of r, self.siblings[r].value for r in currentreqs
+        # reqvalues = a comprehension of r,
+        reqvalues = {self.siblings[r].value for r in currentreqs}
         # Build an environment with these values and basic math functions
-        
+
         environment = ChainMap(math.__dict__, reqvalues)
         # Note that eval is DANGEROUS and should not be used in production
         self.value = eval(self.formula, {}, environment)
 
         # save currentreqs in self.reqs
+        self.requirements = currentreqs
         # set this cell's var to cell's value
-        # 
+        self.var.set(self.value)
 
     def propagate(self):
-        pass 
+        pass
         # for each of your deps
         #     calculate
         #     propogate
@@ -103,6 +108,7 @@ class Cell():
     def load(self, filename):
         pass
 
+
 class SpreadSheet(tk.Frame):
     def __init__(self, rows=5, cols=5, master=None):
         super().__init__(master)
@@ -122,17 +128,17 @@ class SpreadSheet(tk.Frame):
         blank = tk.Label(self.cellframe)
         blank.grid(row=0, column=0)
         for j in range(self.cols):
-            label = tk.Label(self.cellframe, text=chr(ord('A')+j))
-            label.grid(row=0, column=j+1)
+            label = tk.Label(self.cellframe, text=chr(ord('A') + j))
+            label.grid(row=0, column=j + 1)
 
         # Fill in the rows
         for i in range(self.rows):
             rowlabel = tk.Label(self.cellframe, text=str(i + 1))
-            rowlabel.grid(row=1+i, column=0)
+            rowlabel.grid(row=1 + i, column=0)
             for j in range(self.cols):
                 cell = Cell(i, j, self.cells, self.cellframe)
                 self.cells[cell.name] = cell
-                cell.widget.grid(row=1+i, column=1+j)
+                cell.widget.grid(row=1 + i, column=1 + j)
 
 
 root = tk.Tk()
